f0bee4dec934390d5295d07ca0364f728e5fedb9
RON_ALLOGO <ronallogo45@gmail.com>
2024-08-11 03:45:19 +0200

    commit 11/08/2024

---
+++ src/app/pages/EmployeeTools/employee-creation/employee-creation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeCreationComponent } from './employee-creation.component';
+
+describe('EmployeeCreationComponent', () => {
+  let component: EmployeeCreationComponent;
+  let fixture: ComponentFixture<EmployeeCreationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EmployeeCreationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmployeeCreationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/EmployeeTools/employee-creation/employee-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-creation.component.html',
+  styleUrl: './employee-creation.component.css'
+})
+export class EmployeeCreationComponent {
+
+}
